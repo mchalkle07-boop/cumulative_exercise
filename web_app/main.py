6c51f78d1fcbf9342704e1b11dd2f31459e8db57
mchalkle07-boop <mchalkle07@gmail.com>
2026-08-19 08:26:03 -0700

    corrections to main.py

--- web_app/main.py
+++ web_app/main.py
@@ -1,3 +1,4 @@
+import requests  # Fixed: Added missing import
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -8,19 +9,23 @@ async def index():
     return {"message": "Hello World"}
 
 
+# Fixed: Added string type hints to the path parameters
 @app.post("/convert/{state}/{city}")
-def convert(state, city):
+def convert(state: str, city: str):
     print("Converting lat long")
+
     lat = None
     long = None
-
     api_key = "6a7f57138e04b429290986wxia11d90"
+
+    # Fixed: Added quotes to the dictionary keys
     payload = {"api_key": api_key, "state": state, "city": city}
 
     response = requests.get("https://geocode.maps.co/search", params=payload)
+
     best_result = response.json()[0]
     lat = best_result["lat"]
     long = best_result["lon"]
-    result = {"lat": lat, "long": long}
 
+    result = {"lat": lat, "long": long}
     return result
